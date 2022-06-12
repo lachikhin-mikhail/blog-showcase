57487874722ac18fe5c0a88ca33547eb76943e75
Misha <misha.lachikhin@gmail.com>
2022-06-12 16:00:41 +0300

    Changed SQLite to postgres and made a first migration

--- blog/settings.py
+++ blog/settings.py
@@ -72,11 +72,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog-showcase',
+        'USER':'postgres',
+        'PASSWORD':'2802',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
