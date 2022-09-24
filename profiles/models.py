979663bde47edb1fa72a6d713b8c1750f275285d
Misha <misha.lachikhin@gmail.com>
2022-09-24 09:53:51 +0300

    Now profile is created and tied to new user everytime new user signing up

--- profiles/models.py
+++ profiles/models.py
@@ -5,14 +5,22 @@ from stdimage.models import StdImageField
 
     
 class Profile(models.Model):
-    """Fields that can be used for filling the profile page"""
+    """Fields that can be used to modify the profile page"""
     name = models.CharField(max_length=50)
     bio = models.TextField(max_length=160)
     profilePicture = StdImageField(upload_to='profileImages/')
     
     """Technical fields unaccessable for users"""
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
-    likes = models.ForeignKey("posts.Post", on_delete=models.CASCADE, related_name='posts_liked')
-    posts = models.ManyToManyField("posts.Post", related_name='posts_made')
-    following = models.ForeignKey(User, on_delete=models.CASCADE)
-    # favTags = models.ForeignKey("app.Model", on_delete=models.CASCADE) # to be implemented later
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
+    likes = models.ForeignKey("posts.Post", on_delete=models.SET_NULL, related_name='posts_liked',null=True, blank=True)
+    
+    # Here I want to make "posts" category, so user can also repost others posts
+    # so pulling someone's posts by "author" field from "post" model wont be good enough
+    posts = models.ForeignKey("posts.Post", on_delete=models.SET_NULL, related_name='posts_made',null=True, blank=True)
+    following = models.ForeignKey("self", on_delete=models.SET_NULL, null=True, blank=True)
+    
+    def __str__(self):
+        str=self.owner.username
+        str=str + "'s profile"
+        return str
+        
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
+from profiles.models import Profile
 
 
 """ def signup(request):
@@ -23,19 +24,19 @@ from django.contrib import auth
 
 def signup(request):
     if request.method == 'POST':
-        # User has info and wants an account now!
         if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.get(username=request.POST['username'])
-                return render(request, 'signup.html', {'error': 'Username has already been taken'})
+                return render(request, 'signup.html', {'error': 'Username has already been taken ☹️'})
             except User.DoesNotExist:
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(request.POST['username'],
+                                                password=request.POST['password1'])
+                profile = Profile.objects.create(owner=User.objects.get(username=request.POST['username']))
                 auth.login(request, user)
                 return redirect('home')
         else:
             return render(request, 'signup.html', {'error': 'Passwords must match'})
     else:
-        # User wants to enter info
         return render(request, 'signup.html')
 
 def login(request):
